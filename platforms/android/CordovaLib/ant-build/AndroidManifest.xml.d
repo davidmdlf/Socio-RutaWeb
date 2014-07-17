1c53bd8f2f6e61cef681436d5901ad5474e64d61
davidmdlf <david.mdlf@gmail.com>
2014-07-17 11:32:54 +0200

    Socio-Ruta Web v1.0 Primera release Proyecto Cordova completo con proyecto para plataforma Android

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\David\Documents\NetBeansProjects\Socio-RutaWeb\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\David\Documents\NetBeansProjects\Socio-RutaWeb\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Users\David\Documents\NetBeansProjects\Socio-RutaWeb\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
